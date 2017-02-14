99e68ab84e627d17ac368c3a1cfd0bdbd6dd1843
rilmanigit <risto.ilmaniemi@student.saimia.fi>
2017-02-14 10:38:50 +0200

    Added hello world

---
+++ hello-world/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Comment line begins with //
+            // My first program with Visual Studio
+            // Print Line
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
